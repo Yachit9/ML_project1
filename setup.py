b5ef7af7238f3f8f67ab6779637c07795bbeba51
YACHIT MAHAJAN <113870482+Yachit9@users.noreply.github.com>
2024-07-15 14:38:42 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-name='mlproject',
+name='ML_project1',
 version='0.0.1',
 author='Yachit',
 author_email='mahajanyachit@gmail.com',
